0e477e5f2903ae7751f9f36020c2ca149b2b05c8
+ <+>
2023-04-25 13:55:28 -0400

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(names, string) {
+    const listofDrivers = [];
+    for (const names of drivers) {
+        if (string === drivers)
+        {
+            return listofDrivers.push(drivers)
+        }
+    }
+    return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(drivers, letter)
+{
+    return drivers.filter(drivers => 
+        drivers.startsWith(letter))
+}
+
+function matchName(drivers,name) {
+    return drivers.filter(driver => driver.name === name)
+}
